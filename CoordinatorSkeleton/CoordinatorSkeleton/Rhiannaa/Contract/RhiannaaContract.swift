5c493eec51e797b6acb9fc91e942a9e873d451c6
RhiannaaSingh <rhiannaa22@hotmail.com>
2020-04-22 14:52:34 +0100

    Rhianna Tab
    Add a tab to my list

---
+++ CoordinatorSkeleton/CoordinatorSkeleton/Rhiannaa/Contract/RhiannaaContract.swift
@@ -0,0 +1,25 @@
+//
+//  RhiannaaCoordinator.swift
+//  RecipeApp
+//
+//  Created by Rhiannaa on 22/04/2020.
+//  Copyright © 2020 Ilkay Hamit. All rights reserved.
+//
+
+import Foundation
+
+protocol RhiannaaCoordinatorProtocol: class {
+    
+}
+
+protocol RhiannaaPresenterProtocol: class {
+    
+}
+
+protocol RhiannaaViewProtocol: class {
+    
+}
+
+protocol RhiannaaInteractorProtocol: class {
+    
+}
---
+++ CoordinatorSkeleton/CoordinatorSkeleton/Rhiannaa/Interactor/RhiannaaInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  RhiannaaInteractor.swift
+//  RecipeApp
+//
+//  Created by Rhiannaa on 22/04/2020.
+//  Copyright © 2020 Ilkay Hamit. All rights reserved.
+//
+
+import Foundation
+
+class RhiannaaInteractor: RhiannaaInteractorProtocol {
+    
+}
