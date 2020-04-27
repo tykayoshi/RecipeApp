ce34b40d069aa744ae8d456efd2498f751be9d94
RhiannaaSingh <rhiannaa22@hotmail.com>
2020-04-27 16:12:37 +0100

    Signup and RegisterName
    Signup and RegisterName added

---
+++ CoordinatorSkeleton/CoordinatorSkeleton/Signup/RegisterName/View/RegisterNameViewController.swift
@@ -0,0 +1,11 @@
+//
+//  RegisterNameViewController.swift
+//  RecipeApp
+//
+//  Created by Rhiannaa on 27/04/2020.
+//  Copyright © 2020 Ilkay Hamit. All rights reserved.
+//
+
+import Foundation
+// ViewController
+//sdasddsadasdasdas
