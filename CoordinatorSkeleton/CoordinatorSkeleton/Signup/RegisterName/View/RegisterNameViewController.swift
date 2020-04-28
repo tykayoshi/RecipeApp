1fa328a0b8afc582d6e62c422a9f33deef5ea4f8
Natalie <nattadey@yahoo.co.uk>
2020-04-28 13:32:47 +0100

    completed registerName screen and keyboard

--- CoordinatorSkeleton/CoordinatorSkeleton/Signup/RegisterName/View/RegisterNameViewController.swift
+++ CoordinatorSkeleton/CoordinatorSkeleton/Signup/RegisterName/View/RegisterNameViewController.swift
@@ -13,9 +13,37 @@ class RegisterNameViewController: UIViewController {
     
     var presenter: RegisterNamePresenterProtocol!
     
+    @IBOutlet weak var NameTextField: RATextField!
+    @IBOutlet weak var AgeTextField: RATextField!
+    @IBOutlet weak var BottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textFieldPadding(textField: NameTextField)
+        textFieldPadding(textField: AgeTextField)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(with:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
     }
+    
+    @objc func keyboardWillShow(with notification: Notification) {
+        let key = "UIKeyboardFrameEndUserInfoKey"
+        guard let keyboardFrame = notification.userInfo?[key] as? NSValue else {return}
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        
+        BottomConstraint.constant = keyboardHeight
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func textFieldPadding(textField: RATextField!) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = UITextField.ViewMode.always
+    }
+    
     
     @IBAction func backButtonPressed(_ sender: Any) {
         presenter.backButtonPressed()
