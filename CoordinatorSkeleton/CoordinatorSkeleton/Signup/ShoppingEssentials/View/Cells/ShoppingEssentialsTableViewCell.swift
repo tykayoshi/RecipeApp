33f256a44eaa9e4babe2e2faf6c5a9eea55cbf64
Ilkay <ilkay.hamit@gmail.com>
2020-05-01 12:41:05 +0100

    add custom cells example

---
+++ CoordinatorSkeleton/CoordinatorSkeleton/Signup/ShoppingEssentials/View/Cells/ShoppingEssentialsTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingEssentialsTableViewCell.swift
+//  RecipeApp
+//
+//  Created by Ilkay Hamit on 01/05/2020.
+//  Copyright © 2020 Ilkay Hamit. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingEssentialsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var shoppingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CoordinatorSkeleton/CoordinatorSkeleton/Signup/ShoppingEssentials/View/ShoppingEssentialsViewController.swift
+++ CoordinatorSkeleton/CoordinatorSkeleton/Signup/ShoppingEssentials/View/ShoppingEssentialsViewController.swift
@@ -9,11 +9,16 @@
 import Foundation
 import UIKit
 
-class ShoppingEssentialsViewController: UIViewController{
+class ShoppingEssentialsViewController: UIViewController {
     var presenter: ShoppingEssentialsPresenterProtocol!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let shoppingListNib = UINib(nibName: String(describing: ShoppingEssentialsTableViewCell.self), bundle: nil)
+        tableView?.register(shoppingListNib, forCellReuseIdentifier: String(describing: ShoppingEssentialsTableViewCell.self))
+        
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
@@ -23,6 +28,21 @@ class ShoppingEssentialsViewController: UIViewController{
 
 extension ShoppingEssentialsViewController: ShoppingEssentialsViewProtocol {
     
+}
+
+
+extension ShoppingEssentialsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ShoppingEssentialsTableViewCell.self), for: indexPath) as! ShoppingEssentialsTableViewCell
+    
+        cell.shoppingLabel.text = "Shopping"
+        
+        return cell
+    }
 }
 
 extension ShoppingEssentialsViewController {
